6053aebf9cc5b9925b7fb8f6ef403c204d153e57
Santosh Patil <santosh.patil@powerschool.com>
2020-10-14 20:45:09 +0530

    Updates

---
+++ precache-manifest.4205d2528243a09a9cf025dc019102b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11c8ae1f5a28e673b58d90f3787cb6b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "dedefb5f3e901f8d5940",
+    "url": "/monster-rolodex/static/css/main.0bf1fb18.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dedefb5f3e901f8d5940",
+    "url": "/monster-rolodex/static/js/main.3fa9983c.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
